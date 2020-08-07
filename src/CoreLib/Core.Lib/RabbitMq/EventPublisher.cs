c5d90af084447a485db0219f26da11e2b783158a
moshi <moshiur@cefalo>
2020-08-07 16:35:00 +0600

    RabbitMq message publisher

---
+++ src/CoreLib/Core.Lib/RabbitMq/EventPublisher.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core.Lib.RabbitMq
+{
+    public class EventPublisher : IEventPublisher
+    {
+        private readonly IPublishEndpoint _endpoint;
+
+        public EventPublisher(IPublishEndpoint endpoint)
+        {
+            _endpoint = endpoint;
+        }
+        public async Task Publish<T>(T message, CancellationToken cancellationToken = default) where T : IntegrationEvent
+        {
+            await this._endpoint.Publish<T>(message);
+        }
+
+        public async Task Publish(object message, Type messageType, CancellationToken cancellationToken = default)
+        {
+            await this._endpoint.Publish(message, messageType);
+        }
+    }
+}
---
+++ src/CoreLib/Core.Lib/RabbitMq/IEventPublisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core.Lib.RabbitMq
+{
+    public interface IEventPublisher
+    {
+        public Task Publish<T>(T message, CancellationToken cancellationToken = default) where T : IntegrationEvent;
+
+        public Task Publish(object message, Type messageType, CancellationToken cancellationToken = default);
+    }
+}
