c72c1e667d6fe47479b4d6d28d4889f5b74bce3a
moshi <moshiur@cefalo>
2020-08-12 16:55:55 +0600

    new registered user event consumed and data saved in kyc database

--- src/Services/Kyc/Kyc.API/Kyc.API/Application/IntegrationEvents/UserCreatedIntegratedEventConsumer.cs
+++ src/Services/Kyc/Kyc.API/Kyc.API/Application/IntegrationEvents/UserCreatedIntegratedEventConsumer.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Lib.IntegrationEvents;
+using Kyc.Domain.AggregateModel;
 using MassTransit;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
@@ -8,15 +9,22 @@ namespace Kyc.API.Application.IntegrationEvents
     public class UserCreatedIntegratedEventConsumer : IConsumer<IUserCreatedIntegrationEvent>
     {
         private readonly ILogger<UserCreatedIntegratedEventConsumer> logger;
+        private readonly IKycRepository kycRepository;
 
-        public UserCreatedIntegratedEventConsumer(ILogger<UserCreatedIntegratedEventConsumer> logger)
+        public UserCreatedIntegratedEventConsumer(ILogger<UserCreatedIntegratedEventConsumer> logger,
+            IKycRepository kycRepository)
         {
             this.logger = logger;
+            this.kycRepository = kycRepository;
         }
         public async Task Consume(ConsumeContext<IUserCreatedIntegrationEvent> context)
         {
             IUserCreatedIntegrationEvent message = context.Message;
             this.logger.Log(LogLevel.Information, $"user registred event fired on kyc {message.UserGuid}");
+            var user = new User(message.UserGuid, false);
+
+            await this.kycRepository.Add(user);
+            await this.kycRepository.UnitOfWork.SaveEntitiesAsync();
         }
     }
 }
--- src/Services/Kyc/Kyc.API/Kyc.Domain/AggregateModel/User.cs
+++ src/Services/Kyc/Kyc.API/Kyc.Domain/AggregateModel/User.cs
@@ -1,10 +1,21 @@
 ﻿using Kyc.Domain.SeedWork;
 using System;
+using System.Runtime.CompilerServices;
 
 namespace Kyc.Domain.AggregateModel
 {
     public class User : Entity, IAggregateRoot
     {
         public bool IsKycVerified { get; set; }
+
+        public User()
+        {
+
+        }
+        public User(Guid userId, bool isKycVerified)
+        {
+            this.Id = userId;
+            this.IsKycVerified = isKycVerified;
+        }
     }
 }
