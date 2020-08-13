7882c69dae08ecf8104fb2b0528106aa6948ccce
moshi <moshiur@cefalo>
2020-08-13 17:21:05 +0600

    kyc approved event

---
+++ src/CoreLib/Core.Lib/IntegrationEvents/IKycApprovedEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Lib.IntegrationEvents
+{
+    public interface IKycApprovedEvent
+    {
+        Guid UserId { get; set; }
+    }
+}
---
+++ src/CoreLib/Core.Lib/IntegrationEvents/KycApprovedEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Lib.IntegrationEvents
+{
+    public class KycApprovedEvent : IKycApprovedEvent
+    {
+        public Guid UserId { get ; set ; }
+    }
+}
