cf02a87a64840f10e2c0693e0c03f2c5d192e0e9
moshi <moshiur@cefalo>
2020-08-28 10:59:32 +0600

    deployment script removing extra files and adding shellscript to automate

---
+++ deploy/deploy-all.ps1
@@ -0,0 +1,19 @@
+Param(
+    [parameter(Mandatory=$true)][string]$namespace
+)
+
+#kubectl apply -f sqlserver/secret.yaml --namespace $namespace
+# kubectl apply -f sqlserver/sqlserver-db-data-claim.yaml --namespace $namespace
+# kubectl apply -f sqlserver/sqlsvr-sqlsvr-deployment.yaml --namespace $namespace
+# kubectl apply -f sqlserver/sqlsvr-service.yaml --namespace $namespace
+
+Write-Output "Creating rabbitmq release with admin user: user and password: Password123"
+helm install rabbit-release -f rabbitMq/values.yaml bitnami/rabbitmq --namespace $namespace
+
+Write-Output "Creating configmap for NID server"
+kubectl  create configmap json-server-config --from-file=kyc/db.json --namespace $namespace
+
+Write-Output "Deploying linkerd to $namespace"
+kubectl get --namespace $namespace deploy -o yaml | linkerd inject - | kubectl apply -f -
+
+
